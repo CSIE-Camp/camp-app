41fa489efdd61cb5948002eccbb6899ea0b3fb21
JacobLinCool <jacoblincool@gmail.com>
2024-04-18 23:01:01 +0800

    fix: page transition has no context on server

--- src/routes/PageTransition.svelte
+++ src/routes/PageTransition.svelte
@@ -2,13 +2,21 @@
 	import { afterNavigate, beforeNavigate } from "$app/navigation";
 	import { page } from "$app/stores";
 	import { fade } from "svelte/transition";
+	import { browser } from "$app/environment";
 
 	let show_content = true;
 	beforeNavigate(() => (show_content = false));
 	afterNavigate(() => (show_content = true));
+
+	let pathname = "";
+	if (browser) {
+		page.subscribe((value) => {
+			pathname = value.url.pathname;
+		});
+	}
 </script>
 
-{#key $page.url.pathname}
+{#key pathname}
 	{#if show_content}
 		<div
 			class="h-full w-full"
